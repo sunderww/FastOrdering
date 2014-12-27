906f01fbe93c610ebebdbb8fae15ba0a44976e5a
MewSoul <mewen.page@gmail.com>
2014-12-28 00:05:10 +1100

    History in progress

--- app/src/main/java/com/eip/fastordering/HistoryFragment.java
+++ app/src/main/java/com/eip/fastordering/HistoryFragment.java
@@ -1,12 +1,13 @@
 package com.eip.fastordering;
 
 import android.app.Activity;
-import android.net.Uri;
-import android.os.Bundle;
 import android.app.Fragment;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 public class HistoryFragment extends Fragment {
@@ -15,6 +16,7 @@ public class HistoryFragment extends Fragment {
      * fragment.
      */
     private static final String ARG_SECTION_NUMBER = "section_number";
+
 
     /**
      * Returns a new instance of this fragment for the given section
@@ -35,6 +37,14 @@ public class HistoryFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_history, container, false);
+
+        String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
+                "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+                "Linux", "OS/2" };
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(container.getContext(), R.layout.row_history, R.id.history_label_one, values);
+        ListView lv = (ListView)rootView.findViewById(R.id.history_list);
+        lv.setAdapter(adapter);
+
         return rootView;
     }
 
@@ -43,6 +53,10 @@ public class HistoryFragment extends Fragment {
         super.onAttach(activity);
         ((Main) activity).onSectionAttached(
                 getArguments().getInt(ARG_SECTION_NUMBER));
+    }
+
+    private void addOrderToList() {
+
     }
 
 }
