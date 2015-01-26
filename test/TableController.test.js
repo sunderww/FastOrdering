c77208c6ece1b317231f05972fb2f7afea3ac3f1
hiks <epitech>
2015-01-26 22:06:15 +0100

    UnitTesting: Add Unit Testing
    This is just to add the Unit Tests
    
    Signed-off-by: hiks <epitech>

---
+++ test/TableController.test.js
@@ -0,0 +1,29 @@
+var request = require('supertest');
+var assert = require('assert');
+var Sails = require('sails');
+var app;
+
+describe('TableController', function() {
+    describe('#Table()', function() {
+	it('Table list', function (done) {
+	    request(sails.hooks.http.app)
+		.post('/table/read')
+		.send()
+		.expect(200)
+		.end(function (err, res) {
+		    if(err) return done(err);
+		    done();
+		});
+	});
+//	it('create an element', function (done) {
+//            request(sails.hooks.http.app)
+//              .post('/Order/create')
+//              .send({:"dessert"})
+//              .expect(200)
+//              .end(function (err, res) {
+//		  if(err) return done(err);
+//                  done();
+//           });
+//	});
+    });
+});
