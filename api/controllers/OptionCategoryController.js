fafce97665f7036282b102bc6f6b6bad5e02c991
Alexis Semren <semren_a@epitech.eu>
2015-08-06 11:55:22 +0100

    optioncategory

---
+++ api/controllers/OptionCategoryController.js
@@ -0,0 +1,81 @@
+/**
+ * OptionCategoryController
+ *
+ * @description :: Server-side logic for managing Optioncategories
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+
+  /**
+   * `OptionCategoryController.create()`
+   */
+  create: function (req, res) {
+    if (req.method=="POST" && req.param("id") == undefined) {
+   	OptionCategory.create({
+   		name:req.param("name")
+   		}).exec(function(err, model){
+     		if (err)
+     			return res.json({message: err.ValidationError});
+     		else
+     			return res.json({message: "GG"});
+   	});
+    }
+      OptionCategory.find( function(err, doc) {
+          return res.view({optioncategories:doc});
+        });      
+  },
+
+
+  /**
+   * `OptionCategoryController.read()`
+   */
+  read: function (req, res) {
+    if (req.param("id")) {
+        OptionCategory.find({id: req.param("id")} ,function(err, doc) {
+          return res.send(doc);
+      });
+    } else {
+        OptionCategory.find( function(err, doc) {
+          return res.json({elements: doc});
+      });
+    }
+  },
+
+
+  /**
+   * `OptionCategoryController.delete()`
+   */
+  delete: function (req, res) {
+        OptionCategory.destroy({id:req.param("id")}).exec(function(err, doc) {
+           res.json({elements: doc});
+      });
+      res.redirect(307, '/optioncategory/create');
+  },
+
+
+  /**
+   * `OptionCategoryController.update()`
+   */
+  update: function (req, res) {
+    if (req.method=="POST") {
+     
+      OptionCategory.update({id:req.param("id")},{
+        name:req.param("name"),
+          }).
+      exec(function(err,model) {
+            if (err) {
+          return res.json({
+            message: err.ValidationError
+              });
+            }
+
+      });
+      res.redirect(307, '/optioncategory/create/');
+    }
+    OptionCategory.findOne({id: req.param("id")} ,function(err, doc) {
+          return res.view({option:doc});
+      }); 
+  }
+};
+
