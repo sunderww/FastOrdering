e6731a40164757e5960dcb1ab17efe42bfb69b6e
exploit <exploit@template.labeip.epitech.eu>
2015-09-09 23:34:15 +0200

    socket

---
+++ api/services/socket.js
@@ -0,0 +1,18 @@
+module.exports = {
+
+/*    var _SOCKET = function(){
+	var sockets = [];
+
+	function addSocket(key, socketId) {
+	    this.sockets.push;
+	}
+
+	function getIdSocket(key) {
+	    this.sockets.forEach(entry) {
+		if (entry.key == key)
+		    return entry.socketId;
+	    }
+	}
+    };
+*/
+}
