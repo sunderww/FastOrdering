2e2bdd8ff76ff4f593c946e6f4c3f4220f3de973
exploit <exploit@template.labeip.epitech.eu>
2016-01-13 23:08:52 +0100

    wait

--- config/connections.js
+++ config/connections.js
@@ -64,7 +64,7 @@ module.exports.connections = {
 //    port: 27017,
      user: '',
      password: '',
-      database: 'FastOrdering4',
+      database: 'FastOrdering_v2',
       schema: true
   },
 
