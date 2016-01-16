e3631306db2580bd4c5b61eb7655c4969541200a
Alexis Semren <semren_a@epitech.eu>
2016-01-16 01:08:29 +0100

    improve menu creation

--- api/services/MenuServices.js
+++ api/services/MenuServices.js
@@ -10,12 +10,15 @@ module.exports = {
 			DishCategory.find({restaurant:req.session.user.restaurant})
 		])
 		.spread(function(menuComposition, allComposition, menu, dishCategory){
-			if (req.param('menu_name') != undefined)
+			if (req.param('menu_name') != undefined) {
+				data = true;
 				Menu.update({id:req.param('id')}, {name:req.param('menu_name'), price:req.param('price')}, function(){});
-			data = {"dish_category":dishCategory, "compositions":allComposition, "selected_compositions":menuComposition, "menu": menu};
+			}
+			else
+				data = {"dish_category":dishCategory, "compositions":allComposition, "selected_compositions":menuComposition, "menu": menu};
 		})
 		.catch(function(err){
-			cb(false, err);
+			cb([false, err]);
 		})
 		.done(function(){
 			cb([true, data]);
